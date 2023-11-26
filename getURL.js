97a634da09f29fa7362d6a026afccc4898f6c021
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2023-11-26 02:55:18 -0300

    Add files via upload

--- getURL.js
+++ getURL.js
@@ -1,27 +1,70 @@
 // getURL.js
+const avatar ="https://tr.rbxcdn.com/58ff075820895bda7b0422f7e7f3b4fa/420/420/Hat/Png"; 
+// comentariosArray[i].nome   comentariosArray[i].comentario
+function criarComentario(imagem,name,comment) {
+    var container = document.getElementById("container")
+    
+    var divElement = document.createElement("div");
+    divElement.className = "my-3 p-3 bg-white rounded shadow-sm";
+
+    var h6Element = document.createElement("h6");
+    h6Element.className = "border-bottom border-gray pb-2 mb-0";
+
+
+    var mediaDivElement = document.createElement("div");
+    mediaDivElement.className = "media text-muted pt-3";
+
+    var imgElement = document.createElement("img");
+    imgElement.setAttribute("data-src", "holder.js/32x32?theme=thumb&amp;bg=007bff&amp;fg=007bff&amp;size=1");
+    imgElement.alt = "32x32";
+    imgElement.className = "mr-2 rounded";
+    imgElement.src = imagem;
+    imgElement.setAttribute("data-holder-rendered", "true");
+    imgElement.style.width = "32px";
+    imgElement.style.height = "32px";
+
+    var pElement = document.createElement("p");
+    pElement.className = "media-body pb-3 mb-0 small lh-125 border-bottom border-gray";
+
+    var strongElement = document.createElement("strong");
+    strongElement.className = "d-block text-gray-dark";
+    strongElement.textContent =name;
+
+    var textNode = document.createTextNode(comment);
+
+    // Adicionar elementos ao DOM
+    container.appendChild(divElement);
+    divElement.appendChild(h6Element);
+    divElement.appendChild(mediaDivElement);
+    mediaDivElement.appendChild(imgElement);
+    mediaDivElement.appendChild(pElement);
+    pElement.appendChild(strongElement);
+    pElement.appendChild(textNode);
+}
 
 // Função para obter o parâmetro da URL por nome
 function getURLParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
+const projetoParam = getURLParameter('projeto');
 
 // Função para redirecionar com base no valor do parâmetro 'projeto'
 function redirectToAPIRoute() {
-    const projetoParam = getURLParameter('projeto');
+
 
     if (projetoParam) {
         const apiUrl = 'http://192.168.2.106:5000/';
 
         switch (projetoParam) {
             case '1':
-                makeApiRequest(apiUrl + 'City_Sewer');
+                makeApiRequest(apiUrl + 'City-Sewer');
                 break;
             case '2':
-                makeApiRequest(apiUrl + 'Water_Responsible');
+                makeApiRequest(apiUrl + 'Water-Responsible');
                 break;
             case '3':
-                makeApiRequest(apiUrl + 'Solid_Waste');
+                makeApiRequest(apiUrl + 'Solid-Waste');
                 break;
             default:
                 console.error('Projeto não reconhecido:', projetoParam);
@@ -37,21 +80,87 @@ function makeApiRequest(apiUrl) {
         url: apiUrl,
         type: 'GET',
         dataType: 'json',
-        success: function(data) {
-            alert(data)
-tagHtml = document.getElementById("introducaoText");
-conteudoHTML = $data.introduction;
-tagHtml.innerHTML(conteudoHTML);
+        success: function (data) {
+
+            var tagHtml0 = document.getElementById("titleText");
+            var conteudoHTML = data.title;
+            tagHtml0.innerHTML = conteudoHTML;
+
+            var tagHtml1 = document.getElementById("introducaoText");
+            var conteudoHTML = data.introduction;
+            tagHtml1.innerHTML = conteudoHTML;
+
+            var tagHtml2 = document.getElementById("DesenvolvimentoText1");
+            var conteudoHTML = data.paragraph1;
+            tagHtml2.innerHTML = conteudoHTML;
+
+            var tagHtml3 = document.getElementById("DesenvolvimentoText2");
+            var conteudoHTML = data.paragraph2;
+            tagHtml3.innerHTML = conteudoHTML;
+
+            var tagHtml4 = document.getElementById("comclusaoText");
+            var conteudoHTML = data.conclusion;
+            tagHtml4.innerHTML = conteudoHTML;
+
+
+            var comentariosArray = data.comentario;
+
+            for (var i = 0; i < comentariosArray.length; i++) {
+                criarComentario(avatar,comentariosArray[i].nome,comentariosArray[i].comentario)
+            }
 
             console.log('Resposta da API:', data);
             // Adicione aqui o código para manipular a resposta JSON conforme necessário
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error('Erro na solicitação à API:', status, error);
         }
     });
 }
 
+function adicionarComentario() {
+    // Obter os valores do formulário
+    var nome = document.getElementById('nome').value;
+    var comentario = document.getElementById('comentario').value;
+    console.log(nome, comentario)
+    // Validar se ambos os campos foram preenchidos
+    if (nome && comentario) {
+        // Criar um objeto com os dados do comentário
+        var dadosComentario = {
+            nome: nome,
+            comentario: comentario,
+            numero: projetoParam
+        };
+
+        // Enviar os dados para a rota http://192.168.2.106:5000/adicionar_comentario
+        $.ajax({
+            url: 'http://192.168.2.106:5000/adicionar_comentario',
+            type: 'POST', // ou 'GET' dependendo do seu servidor
+            contentType: 'application/json',
+            data: JSON.stringify(dadosComentario),
+            success: function (response) {
+                // O servidor pode responder com alguma confirmação, se necessário
+                console.log(response);
+                const name = document.getElementById('nome');
+                const comment = document.getElementById('comentario');
+                criarComentario(avatar,name.value,comment.value)
+
+                // Limpar os campos do formulário
+                name.value = '';
+                comment.value = '';
+
+            },
+            error: function (error) {
+                console.error("Erro na requisição AJAX", error);
+            }
+        });
+    } else {
+        alert("Por favor, preencha todos os campos.");
+    }
+
+}
+
+
 // Chamar a função ao carregar a página
-makeApiRequest();
+
 redirectToAPIRoute();
